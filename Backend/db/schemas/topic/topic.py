a9f87f78831365714570a04c36a174eb73ff7824
Akira <am.1.00@bk.ru>
2024-05-29 16:57:53 +0300

    hotfix

--- Backend/db/schemas/topic/topic.py
+++ Backend/db/schemas/topic/topic.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel, Field
 class TopicSchema(BaseModel):
     title: str = Field(min_length=7)
     description: str | None = Field(default=None, min_length=7)
+    total_hours: float
 
     class Config:
         from_attributes = True
